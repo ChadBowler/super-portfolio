de24b5ced3a6b4f31cdfa6740d8bb2745a00022d
ChadBowler <cbowlerdev@gmail.com>
2024-06-28 17:34:46 -0600

    continued deployment testing

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const projectContainer = document.getElementById('projects');
 const menuList = document.getElementById('menu-list');
 
-// const serviceID = process.env.SERVICE_ID;
-// const templateID = process.env.TEMPLATE_ID;
+const serviceID = process.env.SERVICE_ID;
+const templateID = process.env.TEMPLATE_ID;
 
-// console.log(serviceID);
+console.log(serviceID);
 
-
+(function() {
+    emailjs.init(process.env.PUBLIC_ID);
+})();
 // function changeActive(section) {
 //     for (const li of menuList.children) {
 //         if (li.children[0].classList.contains('active')) {
@@ -86,21 +88,21 @@ for (const project of projects) {
     projectContainer.appendChild(newProject)
 }
 
-// document.getElementById('contact-form').addEventListener('submit', function(event) {
-//     event.preventDefault();
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-//     // const serviceID = env.SERVICE_ID;
-//     // const templateID = env.TEMPLATE_ID;
+    // const serviceID = env.SERVICE_ID;
+    // const templateID = env.TEMPLATE_ID;
 
-//     emailjs.sendForm(serviceID, templateID, this)
-//         .then(() => {
-//             alert('Email sent successfully!');
-//             document.getElementById('email-form').style.display = 'none';
-//         }, (err) => {
-//             alert('Failed to send email. Please try again.');
-//             console.log(JSON.stringify(err));
-//         });
-// });
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            alert('Email sent successfully!');
+            document.getElementById('email-form').style.display = 'none';
+        }, (err) => {
+            alert('Failed to send email. Please try again.');
+            console.log(JSON.stringify(err));
+        });
+});
 
 // function openEmailForm() {
 // document.getElementById('email-form').style.display = 'block';
